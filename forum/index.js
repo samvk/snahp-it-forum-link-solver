f4abca710303eab70bd271d7350c29853dde4de5
Sam Kauffman <samvnkauffman@gmail.com>
2018-11-18 17:02:36 -0500

    add support for multiple links

--- forum/index.js
+++ forum/index.js
@@ -4,24 +4,13 @@ const arrayify = (value) => Array.isArray(value) ? value : [value];
 // forum.snahp.it
 const pageText = document.querySelector('.postbody .content').innerHTML;
 
-const snahpitLinkPattern = /https?:\/\/links.snahp.it\/[a-z\d]{35}/i;
-const megaHashPattern = /#F?![a-z\d]{8}(![a-z\d!-_]+)?/i; // just look for the hash since sometimes that's all that's posted
-const megaLinkBase64Pattern = /aHR0cHM6Ly9[a-z\d]+\={1,2}/i;
+const snahpitLinkPattern = /https?:\/\/links.snahp.it\/[a-z\d]{35}/gi;
+const megaHashPattern = /#F?![a-z\d]{8}(![a-z\d!-_]+)?/gi; // just look for the hash since sometimes that's all that's posted
+const megaLinkBase64Pattern = /aHR0cHM6Ly9[a-z\d]+\={1,2}/gi;
 
-
-const snahpitLink = (pageText.match(snahpitLinkPattern) || [])[0];
-const megaLink = (() => {
-    const result = pageText.match(megaHashPattern);
-    if (result) {
-        return `https://mega.nz/${result[0]}`;
-    }
-})();
-const decodedMegaLink = (() => {
-    const result = pageText.match(megaLinkBase64Pattern);
-    if (result) {
-        return atob(result[0]);
-    }
-})();
+const snahpitLinks = (pageText.match(snahpitLinkPattern) || []);
+const megaLinks = (pageText.match(megaHashPattern) || []).map((result) => `https://mega.nz/${result}`);
+const decodedMegaLinks = (pageText.match(megaLinkBase64Pattern) || []).map((result) => atob(result));
 
 // get addition info that might he helpful for the link
 const username = document.querySelector('.postprofile .username-coloured').textContent;
@@ -32,13 +21,12 @@ const params = {
 }
 
 // build link
-const links = [snahpitLink, megaLink, decodedMegaLink]
-    .filter(Boolean)
+const links = [...snahpitLinks, ...megaLinks, ...decodedMegaLinks]
     .map((link) => `${link}?${Object.entries(params).map(([key, values]) => arrayify(values).map((value) => `${key}=${value}`))}`)
 
 // popup with links
 const $popupNode = `<div class="links-alert">
-    ${links.map((link) => `<a class="postlink" href=${link} alt="">${link}</a>`)}
+    ${links.map((link) => `<a class="postlink" href=${link} alt="">${link}</a>`).join('')}
 </div>`;
 
 document.body.insertAdjacentHTML('beforeend', $popupNode);
