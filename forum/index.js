aea8bf6e820eb4961014fc314be9ce538def9430
Sam Kauffman <samvnkauffman@gmail.com>
2018-11-21 22:32:35 -0500

    add support for megalink passwords I've opted to just pass these in the URL as intended - which the assuption that if there are multiple links, the order will line up. This allows me to skip having a mega.nz content script but it for more fragile

--- forum/index.js
+++ forum/index.js
@@ -12,11 +12,13 @@ const pageText = document.querySelector('.postbody .content').innerHTML;
 const snahpitLinkPattern = /https?:\/\/links.snahp.it\/[a-z\d]{35}/gi;
 const megaHashPattern = /#F?![a-z\d]{8}(![a-z\d!-_]+)?/gi; // just look for the hash since sometimes that's all that's posted
 const megaLinkBase64Pattern = /aHR0cHM6Ly9[a-z\d]+\={1,2}/gi;
+const megaLinkPasswordPattern = /![a-z\d-_]{20,}/gi;
 const zippyshareLinkPattern  = /https?:\/\/www(113)?.zippyshare.com\/v\/[a-z\d-_]+\/file.html/gi;
 const nofileIoLinkPattern  = /https?:\/\/(www.)?nofile.io\/f\/[a-z\d-_]+/gi;
 
 const snahpitLinks = (pageText.match(snahpitLinkPattern) || []);
-const megaLinks = (pageText.match(megaHashPattern) || []).map((result) => `https://mega.nz/${result}`);
+const megaLinkPasswords = (pageText.match(megaLinkPasswordPattern) || []);
+const megaLinks = (pageText.match(megaHashPattern) || []).map((result, i) => `https://mega.nz/${result}${(megaLinkPasswords[i] || '')}`);
 const decodedMegaLinks = (pageText.match(megaLinkBase64Pattern) || []).map((result) => atob(result));
 const zippyshareLinks = (pageText.match(zippyshareLinkPattern) || []);
 const nofileIoLinks = (pageText.match(nofileIoLinkPattern) || []);
