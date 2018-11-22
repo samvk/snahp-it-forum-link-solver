433c0180ce82ae8ee57bcafdf3d30bc465ec007a
Sam Kauffman <samvnkauffman@gmail.com>
2018-11-21 22:20:54 -0500

    add support for zippyshare and nofile.io

--- forum/index.js
+++ forum/index.js
@@ -12,10 +12,14 @@ const pageText = document.querySelector('.postbody .content').innerHTML;
 const snahpitLinkPattern = /https?:\/\/links.snahp.it\/[a-z\d]{35}/gi;
 const megaHashPattern = /#F?![a-z\d]{8}(![a-z\d!-_]+)?/gi; // just look for the hash since sometimes that's all that's posted
 const megaLinkBase64Pattern = /aHR0cHM6Ly9[a-z\d]+\={1,2}/gi;
+const zippyshareLinkPattern  = /https?:\/\/www(113)?.zippyshare.com\/v\/[a-z\d-_]+\/file.html/gi;
+const nofileIoLinkPattern  = /https?:\/\/(www.)?nofile.io\/f\/[a-z\d-_]+/gi;
 
 const snahpitLinks = (pageText.match(snahpitLinkPattern) || []);
 const megaLinks = (pageText.match(megaHashPattern) || []).map((result) => `https://mega.nz/${result}`);
 const decodedMegaLinks = (pageText.match(megaLinkBase64Pattern) || []).map((result) => atob(result));
+const zippyshareLinks = (pageText.match(zippyshareLinkPattern) || []);
+const nofileIoLinks = (pageText.match(nofileIoLinkPattern) || []);
 
 // get addition info that might he helpful for the link
 const username = document.querySelector('.postprofile .username-coloured').textContent;
